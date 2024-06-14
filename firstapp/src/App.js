a6cd7f40a2bf0d047a6fb035d38128857e9d905f
ishav9 <vermaisha7664@gmail.com>
2024-06-14 23:10:28 +0530

    hey

---
+++ firstapp/src/App.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import TodoScreen from "./Screens/TodoScreen";
+class App extends Component{
+    render(){
+        return <TodoScreen/>;
+    }
+}
+export default App;
+
+// CDN stands for content Distribution Network
---
+++ firstapp/src/Screens/TodoScreen.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+class TodoScreen extends Component{
+    render(){
+        return(
+            <h1 className="ui-heading center">To Do List</h1>
+        )
+    }
+}
+export default TodoScreen;
